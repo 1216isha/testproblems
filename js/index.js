f433f4cb15a46d0d83de56bfd843f0e6f60e36a5
1216isha <ishanaveed40@gmail.com>
2024-05-20 12:11:57 -0800

    xyz

--- js/index.js
+++ js/index.js
@@ -9,3 +9,24 @@ let y = 6
 
 let sum = x+y;
 console.log(sum);
+
+
+let x =5
+
+let y = 6 
+
+let sum = x+y;
+console.log(sum);
+
+let x =5
+
+let y = 6 
+
+let sum = x+y;
+console.log(sum);
+let x =5
+
+let y = 6 
+
+let sum = x+y;
+console.log(sum);
